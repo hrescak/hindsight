fcd5eeffc4c7b68c227733c85a61f18a8648e27e
Matej Hrescak <matej@hrescak.com>
2021-03-20 22:08:59 -0400

    buildguide1

---
+++ components/LinkList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Link {
+  description?: string;
+  title: string;
+  URL: string;
+  provider?: "amazon" | "home depot";
+}
+
+interface LinkListProps {
+  title: string;
+  links: Link[];
+}
+
+const LinkList: React.FC<LinkListProps> = ({ links }) => {
+  return (
+    <div>
+      {links.map((link) => (
+        <div key={link.title}></div>
+      ))}
+    </div>
+  );
+};
+
+export default LinkList;
