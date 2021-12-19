5a14db2b5d5f4d32d88f82bdaeb491f1de5bbb84
Matej Hrescak <matej@hrescak.com>
2021-12-19 17:53:59 -0500

    fixes ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ export const getAllPosts = async () => {
   )
     .then((res) => res.json())
     // filter out unpublished posts
-    .then((posts) => posts.filter((post) => post.isPublished))
+    .then((posts) => posts.filter((post: any) => post.isPublished))
     // order them by date published
     .then((posts) =>
-      posts.sort((a, b) => (a.publishedAt < b.publishedAt ? 1 : -1))
+      posts.sort((a: any, b: any) => (a.publishedAt < b.publishedAt ? 1 : -1))
     );
 };
 
@@ -26,14 +26,14 @@ export async function getStaticProps() {
   };
 }
 
-function HomePage({ posts }) {
+function HomePage({ posts }: { posts: any }) {
   return (
     <Layout>
       <h1 className="text-3xl font-semibold">Hindsight Supply</h1>
       <p className="text-gray-500">More of a kitchen than a showroom.</p>
       <h2 className="text-xl mt-8 mb-2 font-semibold">Posts</h2>
       <ul>
-        {posts.map((post, idx) => (
+        {posts.map((post: any, idx: number) => (
           <li key={idx} className="mb-2">
             <Link href="updates/[slug]" as={`/updates/${post.slug}`}>
               <a>{post.title}</a>
--- pages/updates/[slug].tsx
+++ pages/updates/[slug].tsx
@@ -5,12 +5,16 @@ import Layout from "../../components/Layout";
 import { getAllPosts } from "../index";
 import dayjs from "dayjs";
 
-export async function getStaticProps({ params: { slug } }) {
+export async function getStaticProps({
+  params: { slug },
+}: {
+  params: { slug: String };
+}) {
   // Get all posts again
   const posts = await getAllPosts();
 
   // Find the current blogpost by slug
-  const post = posts.find((t) => t.slug === slug);
+  const post = posts.find((t: any) => t.slug === slug);
 
   const blocks = await fetch(
     `https://notion-api.hindsight.workers.dev/v1/page/${post.id}`
@@ -27,12 +31,12 @@ export async function getStaticProps({ params: { slug } }) {
 export async function getStaticPaths() {
   const posts = await getAllPosts();
   return {
-    paths: posts.map((post) => ({ params: { slug: post.slug } })),
+    paths: posts.map((post: any) => ({ params: { slug: post.slug } })),
     fallback: false,
   };
 }
 
-export default ({ post, blocks }) => (
+export default ({ post, blocks }: { post: any; blocks: any }) => (
   <Layout>
     <header className="mb-8">
       <nav>
