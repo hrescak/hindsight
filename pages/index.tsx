dea17b7582c4c29c628c44f5ac5d38b07c7737e1
Matej Hrescak <matej@hrescak.com>
2021-12-18 19:23:35 -0500

    moved everything to notion because

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,38 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
-import { Update } from "../types";
-import useUpdates from "../hooks/useUpdates";
-interface IndexProps {
-  updates: Update[];
+import Link from "next/link";
+
+const NOTION_BLOG_ID = "50a2fbf6f89640008ba508d870a912fa";
+
+export const getAllPosts = async () => {
+  return await fetch(
+    `https://notion-api.hindsight.workers.dev/v1/table/${NOTION_BLOG_ID}`
+  ).then((res) => res.json());
+};
+
+export async function getStaticProps() {
+  const posts = await getAllPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
-export default function Index({ updates }: IndexProps) {
+function HomePage({ posts }) {
   return (
     <Layout>
       <h1 className="text-3xl font-semibold">Hindsight Supply</h1>
       <p className="text-gray-500">More of a kitchen than a showroom.</p>
       <h2 className="text-xl mt-8 mb-2 font-semibold">Updates</h2>
       <ul>
-        {updates.map((update) => (
-          <li key={update.filePath} className="mb-2">
-            <Link
-              as={`/updates/${update.filePath.replace(/\.mdx?$/, "")}`}
-              href={`/updates/[slug]`}
-            >
-              <a>{update.data.title}</a>
+        {posts.map((post, idx) => (
+          <li key={idx} className="mb-2">
+            <Link href="updates/[slug]" as={`/updates/${post.slug}`}>
+              <div>{post.title}</div>
             </Link>
             <br />
-            <span className="text-sm text-gray-400">
-              {update.data.publishedAt}
-            </span>
+            <span className="text-sm text-gray-400">published at time</span>
           </li>
         ))}
       </ul>
@@ -32,8 +40,4 @@ export default function Index({ updates }: IndexProps) {
   );
 }
 
-export function getStaticProps() {
-  // load the files frontmatter
-  const { updateFileMetas } = useUpdates();
-  return { props: { updates: updateFileMetas } };
-}
+export default HomePage;
